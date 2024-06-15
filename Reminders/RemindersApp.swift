9ead665f9ca75ef9f3030cc2fe521a2d1e105b15
Nikunj Thakur <nikunj.thakur@thescore.com>
2024-06-14 23:07:49 -0400

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Nikunj Thakur on 2024-06-14.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
